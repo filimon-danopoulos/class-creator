d1b02bde5a67b324266efd77ab5d457218deb6d0
Filimon Danopoulos <filimon.danopoulos@gmail.com>
2014-12-17 14:06:19 +0100

    Added clean to grunt build

--- GruntFile.js
+++ GruntFile.js
@@ -21,20 +21,25 @@ grunt.initConfig({
         test: {
             options: {
                 reporter: 'spec',
-                quiet: false, // Optionally suppress output to standard out (defaults to false)
                 clearRequireCache: true // Optionally clear the require cache before running tests (defaults to false)
             },
             src: ['test/**/*.js']
         }
-    }
+    },
+    clean: {
+        build: {
+            src: [ 'app' ]
+        }
+    } 
 });
 
 grunt.loadNpmTasks('grunt-typescript');
 grunt.loadNpmTasks('grunt-mocha-test');
+grunt.loadNpmTasks('grunt-contrib-clean');
 
 // Default task(s).
-grunt.registerTask('default', ['typescript', 'mochaTest']);
-grunt.registerTask('compile', ['typescript']);
+grunt.registerTask('default', ['clean', 'typescript', 'mochaTest']);
+grunt.registerTask('compile', ['clean', 'typescript']);
 grunt.registerTask('test', ['mochaTest']);
 
 };
