30330003fc1e1f1285f9a5a724f2ef9184138003
Filimon Danopoulos <filimon.danopoulos@gmail.com>
2014-12-20 15:00:54 +0100

    Simplyfied tests by removing redundant assertions

--- test/StandardTokenizerTest.js
+++ test/StandardTokenizerTest.js
@@ -8,94 +8,46 @@ describe("StandardTokenizer", function() {
         it("should return a private when the property starts with a _", function() {
             var input = { _TEST: "test" },
                 result = tokenizer.tokenize(input);
-
-            assert(result.length === 1, "should only get a single result");
-            var rootClass = result[0];
-            assert(rootClass.className === "rootClass", "the className should be rootClass");
-            assert(rootClass.tokens.length === 1, "it should have one token");
-            var token = rootClass.tokens[0];
-            assert(token.accessor === "private", "it should have the right accessor");
+            assert(result[0].tokens[0].accessor === "private");
         });   
         it("should return a public when property doesn't start with a _", function() {
             var input = { TEST: "1.0" },
                 result = tokenizer.tokenize(input);
-
-            assert(result.length === 1, "should only get a single result");
-            var rootClass = result[0];
-            assert(rootClass.className === "rootClass", "the className should be rootClass");
-            assert(rootClass.tokens.length === 1, "it should have one token");
-            var token = rootClass.tokens[0];
-            assert(token.accessor === "public", "it should have the right accessor");
+            assert(result[0].tokens[0].accessor === "public");
         }); 
 
         it("should return a constant when property is all upper case", function() {
             var input = { _TEST: "1.0" },
                 result = tokenizer.tokenize(input);
-                
-            assert(result.length === 1, "should only get a single result");
-            var rootClass = result[0];
-            assert(rootClass.className === "rootClass", "the className should be rootClass");
-            assert(rootClass.tokens.length === 1, "it should have one token");
-            var token = rootClass.tokens[0];
-            assert(token.construct === "constant", "it should have the right construct");
+            assert(result[0].tokens[0].construct === "constant");
         }); 
         it("should return a field when property starts a lower case letter", function() {
             var input = { _test: "test" },
                 result = tokenizer.tokenize(input);
-               
-            assert(result.length === 1, "should only get a single result");
-            var rootClass = result[0];
-            assert(rootClass.className === "rootClass", "the className should be rootClass");
-            assert(rootClass.tokens.length === 1, "it should have one token");
-            var token = rootClass.tokens[0];
-            assert(token.construct === "field", "it should have the right construct");
+            assert(result[0].tokens[0].construct === "field");
         });
         it("should return a property when property starts with an upper case letter", function() {
             var input = { _Test: "test" },
                 result = tokenizer.tokenize(input);
-                
-            assert(result.length === 1, "should only get a single result");
-            var rootClass = result[0];
-            assert(rootClass.className === "rootClass", "the className should be rootClass");
-            assert(rootClass.tokens.length === 1, "it should have one token");
-            var token = rootClass.tokens[0];
-            assert(token.construct === "property", "it should have the right construct");
+            assert(result[0].tokens[0].construct === "property");
         });
 
         it("should return a integer when property an integer value", function() {
             var input = { _TEST: 1 },
                 result = tokenizer.tokenize(input);
-            
-            assert(result.length === 1, "should only get a single result");
-            var rootClass = result[0];
-            assert(rootClass.className === "rootClass", "the className should be rootClass");
-            assert(rootClass.tokens.length === 1, "it should have one token");
-            var token = rootClass.tokens[0];
-            assert(token.type === "integer", "it should have the right type");
+            assert(result[0].tokens[0].type === "integer");
         });
         it("should return a string when property has a non-number string value", function() {
             var input = { TEST: "test" },
                 result = tokenizer.tokenize(input);
-             
-            assert(result.length === 1, "should only get a single result");
-            var rootClass = result[0];
-            assert(rootClass.className === "rootClass", "the className should be rootClass");
-            assert(rootClass.tokens.length === 1, "it should have one token");
-            var token = rootClass.tokens[0];
-            assert(token.type === "string", "it should have the right type");
+            assert(result[0].tokens[0].type === "string");
         });
         it("should return a float when property has a float string value", function() {
             var input = { _test: "1.0" },
                 result = tokenizer.tokenize(input);
-              
-            assert(result.length === 1, "should only get a single result");
-            var rootClass = result[0];
-            assert(rootClass.className === "rootClass", "the className should be rootClass");
-            assert(rootClass.tokens.length === 1, "it should have one token");
-            var token = rootClass.tokens[0];
-            assert(token.type === "float", "it should have the right type");
+            assert(result[0].tokens[0].type === "float");
         }); 
-        it(" should handle test case 1", function () {
+        it("should handle test case 1", function () {
             var input = {
                 classOne: {
                     intField: 1,
