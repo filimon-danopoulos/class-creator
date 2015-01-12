ca38d78f0102fd8f393fb7afb2674524b5d10552
Filimon Danopoulos <filimon.danopoulos@gmail.com>
2015-01-12 15:43:13 +0100

    Added TypeScript class template

--- src/app/generators/typescript/TypeScriptClassTemplate.ts
+++ src/app/generators/typescript/TypeScriptClassTemplate.ts
@@ -1,1 +1,17 @@
 // <reference path="../../contracts/index." />
+
+class TypeScriptClassTemplate implements ICodeTemplate {
+    getTemplate(): string {
+        return [
+            "class {{className}} {",
+                "{{members}}",
+            "}"
+        ].join('\n')
+    }        
+    getPlaceHolders(): string[] {
+        var template = this.getTemplate();
+        return template.match(/\{\{(.*?)\}\}/g).map(x => x.slice(2,-2));
+    }
+}
+
+export = TypeScriptClassTemplate; 
---
+++ src/test/TypeScriptTemplateTest.ts
@@ -0,0 +1,25 @@
+// <reference path="../thirdparty/mocha/mocha.d.td" />
+// <reference path="../app/contracts/index.d.ts" />
+
+import assert = require("assert");
+
+import TypeScriptClassTemplate = require("../app/generators/typescript/TypeScriptClassTemplate");
+
+var validPlaceHolders = ["accessor", "construct", "name", "type"];
+
+
+describe("TypeScriptClassTemplate ", function() {
+    describe("getPlaceHolders", function() {
+        it("should only return valid placeholders", function() {
+            var template = new TypeScriptClassTemplate();
+            var placeHolders = template.getPlaceHolders();
+            var validPlaceHolders = ["className", "members"];
+            for (var i = 0, l = placeHolders.length; i < l; i ++) {
+                var current = placeHolders[i];
+                assert(validPlaceHolders.indexOf(current) !== -1, current+" is not a valid placeholder");
+            }
+        });    
+    });
+
+});
+
