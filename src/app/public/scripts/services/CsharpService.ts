1fab1a672ee06c38e23355ebc953a34a0ea542d7
Filimon Danopoulos <filimon.danopoulos@gmail.com>
2015-03-20 15:28:22 +0100

    Updated csharp service

--- src/app/public/scripts/services/CsharpService.ts
+++ src/app/public/scripts/services/CsharpService.ts
@@ -5,22 +5,32 @@ module App.Services {
     }
 
     export class CsharpService extends Main.AngularService implements ICsharpService {
-        public static $inject = ["$http"];
-        constructor(private $http: ng.IHttpService ) {
+        public static $inject = ["$http", "$q"];
+        constructor(private $http: ng.IHttpService, private $q: ng.IQService ) {
             super()
         }
         public static serviceName: string = "csharpService";
 
         public getCodeStringFromJSON(method: Common.ServiceMethod, json: string): ng.IPromise<string> {
+            var deferred = this.$q.defer(),
+                success = resp => deferred.resolve(resp.data),
+                error = err => deferred.reject(err.Status+": "+err.StatusText);
             switch (method) {
                 case Common.ServiceMethod.GET:
-                    return this.$http.get("/api/csharp/string/", {
-                        params: {
-                            json: json
-                        }
-                    });
-                default: throw "Not Implemented";
+                    this.getCsharpStringFromJSON(json).then(success, error);
+                    break;
+                default: throw new Error("Not Implemented");
             }
+            return deferred.promise;
+        }
+
+
+        private getCsharpStringFromJSON(json: string): ng.IPromise<any> {
+            return this.$http.get("/api/csharp/string/", {
+                params: {
+                    json: json
+                }
+            });
         }
     }
 }
